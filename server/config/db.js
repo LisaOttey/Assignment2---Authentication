b31a55a2ed4ecc172aa0b25d7f349f4394bb099b
LisaOttey <71458542+LisaOttey@users.noreply.github.com>
2021-02-28 22:29:24 -0500

    Add files via upload

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI":"mongodb://localhost/contacts"
+    //"URI":"mongodb://localhost/contacts"
+    "URI": "mongodb+srv://Lisa:gtk5YY2sFmT5ATaQ@cluster0.ghvhe.mongodb.net/contacts?retryWrites=true&w=majority"
 }
